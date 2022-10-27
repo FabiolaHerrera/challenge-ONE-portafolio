eae0bbe6a2cde1b19d4fe9979895d4b10ae7dd87
fabiolaherrerakovacs <fherrera@kovacs.cl>
2022-10-27 08:14:48 -0300

    100 por ciento y mobile

--- validacion.js
+++ validacion.js
@@ -1,1 +1,16 @@
-//Haz tú validación en javascript acá
+function validar(){
+   var todo_correcto = true;
+  if(document.getElementById('nombre').value.length < 50 ){
+     var todo_correcto = false;
+     alert("Nombre: Máximo 50 caracteres.");
+  }
+  if(document.getElementById('asunto').value.length < 50 ){
+    var todo_correcto = false;
+    alert("Asunto: Máximo 50 caracteres.");
+ }
+ return todo_correcto;
+}
+
+
+
+  
